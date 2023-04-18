269e06a549dc3e348b0b9b1842c6d70acb995c30
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-04-18 20:57:30 +0300

    edit test commit

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -1,12 +1,17 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 class ChromeTest {
 
@@ -35,15 +40,17 @@ class ChromeTest {
 
     @Test
     void formSubmissionTest() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id='city']input")).sendKeys("Москва");
-        driver.findElement(By.cssSelector("input__box")).sendKeys("Иван Иванов");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79169356902");
-        driver.findElement(By.className("checkbox")).click();
-        driver.findElement(By.tagName("button__text")).click();
-        String expected = "Встреча успешно забронирована на 20.04.2023";
-        String actual = driver.findElement(By.cssSelector("icon-button__text")).getText().trim();
-        Assertions.assertEquals (expected, actual);
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 15;
+        Configuration.browserSize = "800x300";
+        open("http://localhost:9999/");
+        $x("[data-test-id='city'] input").setValue("Москва");
+        $x("[data-test-id='name'] input").setValue("Иван Иванов");
+        $x("[data-test-id='phone'] input").setValue("+79169356902");
+        $x("[data-test-id='agreement']").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $(withText("Успешно! Встреча успешно забронирована на 21.04.2023")).shouldBe(visible);
+
     }
 }
 
